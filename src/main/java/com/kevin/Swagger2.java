56fd43e6937ebc9cf72e0a0e0bcd4d5813e1562b
kevinyangss <kevin_yangss@163.com>
2019-05-14 11:25:20 +0800

    swagger2

---
+++ src/main/java/com/kevin/Swagger2.java
@@ -0,0 +1,42 @@
+package com.kevin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2 配置
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                // 是否开启
+                .enable(true).select()
+                // 扫描的路径包
+                .apis(RequestHandlerSelectors.basePackage("com.jiuye"))
+                // 指定路径处理PathSelectors.any()代表所有的路径
+                .paths(PathSelectors.any()).build().pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用 Swagger2 构建 Restful APIs")
+                .description("更多相关详细信息请联系开发人员.")
+                .termsOfServiceUrl("http://www.spring4all.com/")
+                .contact("Developer Group")
+                .version("1.0")
+                .build();
+    }
+
+}
